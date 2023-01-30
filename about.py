39d005faa6e907505f8b08f30125bda045adff4f
sagyndykovaaida <lvnidass@gmail.com>
2023-01-31 01:47:53 +0600

    add info about student

---
+++ about.py
@@ -0,0 +1,12 @@
+student_name = "Aida"
+student_grade = 85
+
+if student_grade >= 90:
+    print(student_name + " has an A grade.")
+elif student_grade >= 80:
+    print(student_name + " has a B grade.")
+elif student_grade >= 70:
+    print(student_name + " has a C grade.")
+else:
+    print(student_name + " has failed.")
+

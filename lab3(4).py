e5204c7b61c32d48f6701449981a648523f2331d
sagyndykovaaida <lvnidass@gmail.com>
2023-02-06 20:52:01 +0600

    add soloutions

--- lab3(4).py
+++ lab3(4).py
@@ -1,5 +1,21 @@
+
+import random
+
+# n=int(input())
+# sum = 0
+# for i in range(1, n + 1):
+#     sum += i
+# for i in range(random.randint(1, n)):
+#     print(i)
+#     for i in range(n-1):
+#         sum-=i
+#     print(sum)
+
+
 n = int(input())
-sum =int( (n+1)*n/2)
-for i in range (1,n ):
-    sum -= int(input())
+sum = 0
+for i in range(1, n + 1):
+    sum += i
+for i in range(n - 1):
+    sum -= int(input(random.randint(1, n)))
 print(sum)
--- new.py
+++ new.py
@@ -1,1 +1,7 @@
-print("hello")
+line, upp, low = input(), 0, 0
+for symb in line:
+    if symb.isalpha() and symb.isupper():
+        upp += 1
+    elif symb.isalpha() and symb.islower():
+        low += 1
+print(line.upper()) if upp > low else print(line.lower())
